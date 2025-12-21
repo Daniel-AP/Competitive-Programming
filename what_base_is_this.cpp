26dc35212310b1be160f40d5194de0314489afbd
Daniel-AP <daniel24.ap@gmail.com>
2025-12-21 15:50:29 -0600

    what_base_is_this

--- what_base_is_this.cpp
+++ what_base_is_this.cpp
@@ -7,9 +7,45 @@ using namespace std;
 // #define MOD 1000000007
 // #define MOD 998244353
 
-void solve() {
+int fromBase(string n, int b) {
 
-    
+    int ans = 0;
+
+    for(char d: n) {
+        
+        int val = 0;
+
+        if('0' <= d && d <= '9') val = d-'0';
+        else val = d-'A'+10;
+
+        ans *= b;
+        ans += val;
+
+        if(val >= b) return -1;
+
+    }
+
+    return ans;
+
+}
+
+void solve(string x, string y) {
+
+    for(int i = 2; i <= 36; i++) {
+        for(int j = 2; j <= 36; j++) {
+
+            int xx = fromBase(x, i), yy = fromBase(y, j);
+
+            if(xx == -1 || yy == -1) continue;
+            if(xx != yy) continue;
+
+            cout << x << " (base " << i << ") = " << y << " (base " << j << ")" << '\n';
+            return;
+
+        }
+    }
+
+    cout << x << " is not equal to " << y << " in any base 2..36" << '\n';
     
 }
 
@@ -18,10 +54,9 @@ signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
 
-    int t = 1;
-    cin >> t;
+    string x, y;
 
-    while(t--) solve();
+    while(cin >> x >> y) solve(x, y);
 
     return 0;
 
